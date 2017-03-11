3698ab660c9ffe07708334d784fbc9c78970af8e
Buka Cakrawala <kaka10garuda@gmail.com>
2017-03-11 09:54:20 -0800

    basic implementation of numpy

---
+++ practice_one_numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+my_list = [1, 2, 3]
+np.array(my_list)
+# >>> array([1, 2, 3])
+
+another_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+np.array(another_list)
+# >>> array([[1, 2, 3],
+#            [4, 5, 6],
+#            [7, 8, 9]])
